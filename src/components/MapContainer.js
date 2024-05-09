67b93140fbf9b31118cf312efce8b74e8765aa65
Özcan Çetin <cetin445@gmail.com>
2024-05-09 12:49:18 +0300

    show all steps

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -7,6 +7,7 @@ import {
   useLoadScript,
 } from "@react-google-maps/api";
 import { useGlobalContext } from "../GlobalContext/GlobalContext";
+import { CoordinateList } from "./CoordinateList";
 
 const containerStyle = {
   width: "100%",
@@ -14,17 +15,20 @@ const containerStyle = {
 };
 
 const MapContainer = () => {
-  const { companyLocation, userLocations, setStockStatus, startDelivery, setShowMessage } = useGlobalContext();
+  const { companyLocation, userLocations, setStockStatus, startDelivery, setShowMessage, setCurrentLocation } = useGlobalContext();
 
   const [directions, setDirections] = useState(null);
   const [routeIndex, setRouteIndex] = useState(0);
   const [carPosition, setCarPosition] = useState(null);
-  console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
   const { isLoaded, loadError } = useLoadScript({
     id: 'google-map-script',
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    // googleMapsApiKey: "AIzaSyBhXNUfNnhDrQrd-JKBGL4CLu1vOByaJWs",
   });
+  const steps = (directions && directions.routes) && directions.routes[0].legs[0].steps.map((item)=>{
+    return(
+      {lat: item.end_location.lat().toFixed(4), lng: item.end_location.lng().toFixed(4)}
+    )
+  })
 
   const simulateRoute = () => {
     if (directions && directions.routes && directions.routes[0]) {
@@ -34,6 +38,7 @@ const MapContainer = () => {
         const nextLocation = nextStep.end_location;
         setRouteIndex((prevIndex) => prevIndex + 1);
         setCarPosition(nextLocation);
+        setCurrentLocation(nextLocation)
         if(routeIndex === (route.legs[0].steps.length -1)){
             setShowMessage(true)
             setStockStatus(prevStockStatus => prevStockStatus - 1);
@@ -44,7 +49,7 @@ const MapContainer = () => {
 
   useEffect(() => {
     if(startDelivery){
-      const interval = setInterval(simulateRoute, 1000);
+      const interval = setInterval(simulateRoute, 10000);
       return () => clearInterval(interval);
     }
   }, [directions, routeIndex, startDelivery]);
@@ -61,16 +66,23 @@ const MapContainer = () => {
     origin: isLoaded && new window.google.maps.Point(0, 0),
     anchor: isLoaded && new window.google.maps.Point(16, 32),
   };
+  const stepRedIcon = {
+    url: require("../assets/red-pin.png"),
+    scaledSize: isLoaded && new window.google.maps.Size(32, 32),
+    origin: isLoaded && new window.google.maps.Point(0, 0),
+    anchor: isLoaded && new window.google.maps.Point(16, 32),
+  };
 
   const directionsCallback = (response) => {
     if (response !== null && response.status === "OK") {
       setDirections(response);
     }
   };
-
   if (loadError) return <div>Error loading Google Maps</div>;
   if (!isLoaded) return <div>Loading...</div>;
   return (
+    <>
+        <CoordinateList coordinates={steps} routeIndex={routeIndex}/>
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={companyLocation}
@@ -97,11 +109,19 @@ const MapContainer = () => {
       )}
       {directions &&
         directions.routes[0].legs[0].steps.map((step, index) => (
-          <Marker key={index} position={step.start_location} icon={stepIcon} />
+          index > routeIndex ?
+          <Marker key={index} position={step.start_location} icon={stepIcon} /> : <Marker key={index} position={step.start_location} icon={stepRedIcon} />
         ))}
+        {directions &&
+  directions.routes[0].legs.slice(1).map((leg, legIndex) => (
+    leg.steps.map((step, stepIndex) => (
+      <Marker key={stepIndex} position={step.start_location} icon={stepIcon} />
+    ))
+  ))}
       {carPosition && <Marker position={carPosition} icon={carIcon} />}
       {(!startDelivery && directions) && <Marker position={companyLocation} icon={carIcon} />}
     </GoogleMap>
+    </>
   );
 };
 
